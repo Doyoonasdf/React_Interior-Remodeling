5385425072406c468da174670313452eb16e8514
Doyoonasdf <won1hana@naver.com>
2023-01-10 10:22:33 +0900

    슬라이드디자인

--- src/components/main/Information.js
+++ src/components/main/Information.js
@@ -1,41 +1,97 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 import Anim from '../../asset/anime';
 
 function Information({ Scrolled, currentPos }) {
 	let scroll = Scrolled - currentPos || 0;
 	scroll < 0 && (scroll = 0);
-	const slider = useRef(null);
+	const [Slides, setSlides] = useState([]);
+	const slider = useRef('');
 
-	const nextSlide = () => {};
+	const init = () => {
+		const panel = slider.current.querySelector('ul');
+		const lis = panel.querySelectorAll('li');
+		const len = lis.length;
+
+		panel.style.width = 100 * len + '%';
+		lis.forEach((el) => (el.style.width = 100 / len + '%'));
+		panel.lastElementChild !== null && panel.prepend(panel.lastElementChild);
+	};
+
+	const nextSlide = () => {
+		const panel = slider.current.children[0];
+
+		new Anim(panel, {
+			prop: 'margin-left',
+			value: '-200%',
+			duration: 500,
+			callback: () => {
+				panel.append(panel.firstElementChild);
+				panel.style.marginLeft = '-100%';
+			},
+		});
+	};
+
+	const prevSlide = () => {
+		const panel = slider.current.children[0];
+
+		new Anim(panel, {
+			prop: 'margin-left',
+			value: '0%',
+			duration: 500,
+			callback: () => {
+				panel.prepend(panel.lastElementChild);
+				panel.style.marginLeft = '-100%';
+			},
+		});
+	};
+
+	useEffect(() => {
+		axios.get(`${process.env.PUBLIC_URL}/DB/slider.json`).then((json) => {
+			setSlides(json.data.slider);
+		});
+	}, []);
+
+	useEffect(() => {
+		init();
+	}, [Slides]);
+
 	return (
 		<section id='information' className='scroll_view'>
 			<div className='inner'>
 				<h2>Special Exhibition</h2>
-				<div
-					className='wrap'
-					style={
-						{
-							// transform: `translateY(-${Scrolled - currentPos}px)`,
-							// opacity: scroll / 300,
-						}
-					}
-				>
-					<div className='pic'>
-						<img src='' alt='' />
-					</div>
-					<div className='txt'>
-						<h2>Vincent van Gogh</h2>
-						<p>
-							Vincent van Gogh (1853–1890) is world famous. Learn about his life, read his letters,
-							or explore his paintings and drawings. Lorem ipsum, dolor sit amet consectetur
-							adipisicing elit. Accusamus consequuntur impedit nulla laborum id deserunt nostrum
-							illum maiores natus expedita!
-						</p>
-						<span>2022.12.24 - 2023.03.26</span>
-						<button>예약하기</button>
-					</div>
+				<div id='slider' ref={slider}>
+					<ul className='panel'>
+						{Slides.map((slide, idx) => {
+							return (
+								<li key={idx}>
+									<div className='wrap'>
+										<div className='pic'>
+											<img
+												src={`${process.env.PUBLIC_URL}/img/slider/${slide.pic}`}
+												alt={slide.No}
+											/>
+										</div>
+										<div className='txt'>
+											<h2>{slide.tit}</h2>
+											<p>{slide.desc}</p>
+											<span>{slide.date}</span>
+											<button>{slide.res}</button>
+										</div>
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+
+					<button className='prev' onClick={prevSlide}>
+						prev
+					</button>
+					<button className='next' onClick={nextSlide}>
+						next
+					</button>
 				</div>
 			</div>
 		</section>
