500ebfe636ce350742361a37d9e918c3bcb31ab9
Doyoonasdf <won1hana@naver.com>
2023-01-11 01:52:19 +0900

    갤러리 css수정

--- src/redux/flickrSlice.js
+++ src/redux/flickrSlice.js
@@ -1,10 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+//새로 받은 아이디와 키
+//  키  41968bb5751272456795594b64f64fb7
+// 아이디 197355893@N03/
 
 export const fetchFlickr = createAsyncThunk('flickr/requestFlickr', async (opt) => {
 	const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
 	const key = '41968bb5751272456795594b64f64fb7';
-	const method_sample = 'flickr.favorites.getList';
+	// const method_sample = 'flickr.favorites.getList';
 	const method_search = 'flickr.photos.search';
 	const method_user = 'flickr.people.getPhotos';
 	const num = 20;
@@ -14,8 +17,8 @@ export const fetchFlickr = createAsyncThunk('flickr/requestFlickr', async (opt) 
 		url = `${baseURL}&method=${method_search}&api_key=${key}&per_page=${num}&tags=${opt.tags}`;
 	if (opt.type === 'user')
 		url = `${baseURL}&method=${method_user}&api_key=${key}&per_page=${num}&user_id=${opt.user}`;
-	if (opt.type === 'sample')
-		url = `${baseURL}&method=${method_sample}&api_key=${key}&per_page=${num}&user_id=${opt.user}`;
+	// if (opt.type === 'sample')
+	// 	url = `${baseURL}&method=${method_sample}&api_key=${key}&per_page=${num}&user_id=${opt.user}`;
 
 	const response = await axios.get(url);
 	return response.data.photos.photo;
