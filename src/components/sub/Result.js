ac6711244938790facb06be052c6d7cef7760393
Doyoonasdf <won1hana@naver.com>
2023-01-13 04:06:18 +0900

    result페이지완료

--- src/components/sub/Result.js
+++ src/components/sub/Result.js
@@ -4,15 +4,31 @@ import { Link } from 'react-router-dom';
 import Layout from '../common/Layout';
 function Result() {
 	return (
-		<Layout name={'result'} h1name={'WELLCOME TO MMA'}>
+		<Layout name={'result'}>
 			<div className='wrap'>
-				<article>
-					<img src={`${process.env.PUBLIC_URL}/img/`} alt='' />
-				</article>
-				<div className='txt'>
-					<h2>회원 가입을 축하합니다!</h2>
-					<p></p>
-					<Link to='/'>VIEW MORE</Link>
+				<div className='textWrap'>
+					<div className='textBox'>
+						<h2>
+							역사와 예술이 살아숨쉬는 MMA전시회입니다<span>회원가입을 축하합니다</span>
+						</h2>
+
+						<div className='openInfo'>
+							<h2>관람시간</h2>
+							<p>
+								월,화,목,금요일
+								<span>10:00 - 18:00</span>
+							</p>
+							<p>
+								토,일요일
+								<span>09:00 - 21:00</span>
+								(18:00-21:00 야간개장)
+								<br />※ 휴관일: 매주 수요일
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className='imgWrap'>
+					<div className='imgBox'></div>
 				</div>
 			</div>
 		</Layout>
